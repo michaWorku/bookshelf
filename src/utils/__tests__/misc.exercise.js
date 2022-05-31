74a9ed9285ac3a1fc1d05c8fe9d7c7992207c132
mikias.wk <mikias@2f-capital.com>
2022-05-31 15:34:47 +0300

    tormat date unittest done

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,6 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from '../misc'
+
+test('formatDate formats the date to look nice', ()=>{
+    expect(formatDate(new Date('July 15, 1995'))).toBe('Jul 95')
+})
 
