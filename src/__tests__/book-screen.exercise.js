f4747fb834a978fb6ccff55756a017cba0e8bb2c
mikias.wk <mikias@2f-capital.com>
2022-06-01 05:39:14 +0300

    create a list item for the book integration test done

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,9 +1,14 @@
 // 🐨 here are the things you're going to need for this test:
 import * as React from 'react'
 import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import {queryCache} from 'react-query'
-import {buildUser, buildBook} from 'test/generate'
 import * as auth from 'auth-provider'
+import {buildUser, buildBook} from 'test/generate'
+import * as usersDB from 'test/data/users'
+import * as booksDB from 'test/data/books'
+import * as listItemsDB from 'test/data/list-items'
+import {formatDate} from 'utils/misc'
 import {AppProviders} from 'context'
 import {App} from 'app'
 
@@ -11,7 +16,12 @@ import {App} from 'app'
 // general cleanup
 afterEach(async () => {
     queryCache.clear()
-    await auth.logout()
+    await Promise.all([
+      auth.logout(),
+      usersDB.reset(),
+      booksDB.reset(),
+      listItemsDB.reset(),
+    ])
   })
   
 // test.todo('renders all the book information')
@@ -40,27 +50,13 @@ afterEach(async () => {
 
 test('renders all the book information', async () => {
     const user = buildUser()
-    window.localStorage.setItem(auth.localStorageKey, 'SOME_FAKE_TOKEN')
+    await usersDB.create(user)
+    const authUser = await usersDB.authenticate(user)
+    window.localStorage.setItem(auth.localStorageKey, authUser.token)
   
-    const book = buildBook()
+    const book = await booksDB.create(buildBook())
     const route = `/book/${book.id}`
     window.history.pushState({}, 'Test page', route)
-  
-    const originalFetch = window.fetch
-    window.fetch = async (url, config) => {
-      if (url.endsWith('/bootstrap')) {
-        return {
-          ok: true,
-          json: async () => ({
-            user: {...user, token: 'SOME_FAKE_TOKEN'},
-            listItems: [],
-          }),
-        }
-      } else if (url.endsWith(`/books/${book.id}`)) {
-        return {ok: true, json: async () => ({book})}
-      }
-      return originalFetch(url, config)
-    }
   
     render(<App />, {wrapper: AppProviders})
   
@@ -93,4 +89,50 @@ test('renders all the book information', async () => {
     ).not.toBeInTheDocument()
     expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
     expect(screen.queryByLabelText(/start date/i)).not.toBeInTheDocument()
+  })
+
+  test('can create a list item for the book', async () => {
+    const user = buildUser()
+    await usersDB.create(user)
+    const authUser = await usersDB.authenticate(user)
+    window.localStorage.setItem(auth.localStorageKey, authUser.token)
+  
+    const book = await booksDB.create(buildBook())
+    const route = `/book/${book.id}`
+    window.history.pushState({}, 'Test page', route)
+  
+    render(<App />, {wrapper: AppProviders})
+  
+    await waitForElementToBeRemoved(() => [
+      ...screen.queryAllByLabelText(/loading/i),
+      ...screen.queryAllByText(/loading/i),
+    ])
+  
+    const addToListButton = screen.getByRole('button', {name: /add to list/i})
+    await userEvent.click(addToListButton)
+    expect(addToListButton).toBeDisabled()
+  
+    await waitForElementToBeRemoved(() => [
+      ...screen.queryAllByLabelText(/loading/i),
+      ...screen.queryAllByText(/loading/i),
+    ])
+  
+    expect(
+      screen.getByRole('button', {name: /mark as read/i}),
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', {name: /remove from list/i}),
+    ).toBeInTheDocument()
+    expect(screen.getByRole('textbox', {name: /notes/i})).toBeInTheDocument()
+  
+    const startDateNode = screen.getByLabelText(/start date/i)
+    expect(startDateNode).toHaveTextContent(formatDate(Date.now()))
+  
+    expect(
+      screen.queryByRole('button', {name: /add to list/i}),
+    ).not.toBeInTheDocument()
+    expect(
+      screen.queryByRole('button', {name: /mark as unread/i}),
+    ).not.toBeInTheDocument()
+    expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
   })
