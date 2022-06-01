4d5f89f9f23bdb728b5294d4e51a4c33068bf974
mikias.wk <mikias@2f-capital.com>
2022-06-01 10:40:58 +0300

    end to end test done

--- cypress/e2e/smoke.exercise.js
+++ cypress/e2e/smoke.exercise.js
@@ -1,30 +1,55 @@
 // 🐨 you'll want a fake user to register as:
-// import {buildUser} from '../support/generate'
+import {buildUser} from '../support/generate'
 
 describe('smoke', () => {
   it('should allow a typical user flow', () => {
     // 🐨 create a fake user
+    const user = buildUser()
     // 🐨 visit '/' (📜 https://docs.cypress.io/api/commands/visit.html)
+    cy.visit('/')
     // 🐨 find the button named "register" and click it
+    cy.findByRole('button', {name: /register/i}).click()
     // 🐨 within the "dialog" find the username and password fields,
     //    type into them the values for your fake user, then click the register
     //    button to submit the form
+    cy.findByRole('dialog').within(()=>{
+      cy.findByRole('textbox', {name: /username/i}).type(user.username)
+      cy.findByLabelText(/password/i).type(user.password)
+      cy.findByRole('button', {name: /register/i}).click()
+    })
     // 📜 https://docs.cypress.io/api/commands/within.html#Syntax
     // 📜 https://docs.cypress.io/api/commands/type.html#Syntax
     //
     // 🐨 within the "navigation", find the link named "discover" and click it
+    cy.findByRole('navigation').within(()=>{
+      cy.findByRole('link', {name: /discover/i}).click()
+    })
     //
     // 🐨 within the "main", type in the "searchbox" the title of a book and hit enter
     //   💰 when using "type" you can make it hit the enter key with "{enter}"
     //   🐨 within the listitem with the name of your book, find the button
     //      named "add to list" and click it.
     //
+    cy.findByRole('main').within(()=>{
+      cy.findByRole('searchbox', {name: /search/i}).type('Voice of war{enter}')
+      cy.findByRole('listitem', {name: /voice of war/i}).within(()=>{
+        cy.findByRole('button', {name: /add to list/i}).click()
+      })
+    })
     // 🐨 click the reading list link in the navigation
     //
     // 🐨 ensure the "main" only has one element "listitem"
     //   💰 https://docs.cypress.io/api/commands/should.html (.should('have.length', 1))
     //   🐨 click the link with the name of the book you added to the list to go to the book's page
     //
+    cy.findByRole('navigation').within(()=>{
+      cy.findByRole('link', {name: /reading list/i}).click()
+    })
+
+    cy.findByRole('main').within(()=>{
+      cy.findByRole('listitem').should('have.length', 1)
+      cy.findByRole('link', {name: /voice of war/i}).click()
+    })
     // 🐨 type in the notes textbox
     // The textbox is debounced, so the loading spinner won't show up immediately
     // so to make sure this is working, we need to wait for the spinner to show up
@@ -38,12 +63,30 @@ describe('smoke', () => {
     // in favor of nice looking stars, so we have to the force option to click.
     // 📜 https://docs.cypress.io/api/commands/click.html#Arguments
     // 🐨 click the 5 star rating radio button
+    cy.findByRole('main').within(()=>{
+      cy.findByRole('textbox', {name: /notes/i}).type('Awosome book')
+      cy.findByLabelText('loading').should('exist')
+      cy.findAllByLabelText('loading').should('not.exist')
+      
+      cy.findByRole('button', {name: /mark as read/i}).click()
+
+      cy.findByRole('radio', {name: /5 stars/i}).click({force: true})
+    })
+
     //
     // 🐨 navigate to the finished books page
+    cy.findByRole('navigation').within(()=>{
+      cy.findByRole('link', {name: /finished books/i}).click()
+    })
     //
     // 🐨 make sure there's only one listitem here (within "main")
     // 🐨 make sure the 5 star rating radio button is checked
     // 🐨 click the link for your book to go to the books page again
+    cy.findByRole('main').within(()=>{
+      cy.findByRole('listitem').should('have.length', 1)
+      cy.findByRole('radio', {name: /5 stars/i}).should('be.checked')
+      cy.findByRole('link', {name: /voice of war/i}).click()
+    })
     //
     // 🐨 remove the book from the list
     // 🐨 ensure the notes textbox and the rating radio buttons are gone
@@ -51,5 +94,19 @@ describe('smoke', () => {
     // 🐨 navigate back to the finished books page
     //
     // 🐨 ensure there are no books in the list
+    cy.findByRole('main').within(()=>{
+      cy.findByRole('button', {name: /remove from list/i}).click()
+      
+      cy.findByRole('textbox', {name: /notes/i}).should('not.exist')
+      cy.findByRole('radio', {name: /5 stars/i}).should('not.exist')
+    })
+
+    cy.findByRole('navigation').within(()=>{
+      cy.findByRole('link', {name: /finished books/i}).click()
+    })
+
+    cy.findByRole('main').within(()=>{
+      cy.findByRole('listitem').should('have.length', 0)
+    })
   })
 })
